3b7e3e0ccd06fdb8909676ba886ad2a86fb618b8
amakuu <amakuzaxx@gmail.com>
2020-01-04 18:26:00 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,11 @@
 Config = {}
-Config.Shoptext = 'Press (E) to open a Clothing Shop'
-Config.BlipName = 'Clothing Shop'
-Config.BlipSprite = 1475879922
-Config.BlipScale = 0.2
-Config.OpenKey = 0xDFF812F9
-Config.Price = 1
+Config.Shoptext = 'Press (E) to open a Clothing Shop' -- Text to open the clothing shop
+Config.BlipName = 'Clothing Shop' -- Blip Name Showed on map
+Config.EnableCommand = false -- Enable/Disable /clothing command
+Config.BlipSprite = 1475879922 -- Clothing shop sprite
+Config.BlipScale = 0.2 -- Blip scale
+Config.OpenKey = 0xDFF812F9 -- Opening key hash
+Config.Price = 1 -- Price for clothes
 Config.Zones = {
 	vector3(-322.25,803.97,116.95), -- VALENTINE
 	vector3(1225.60,-1293.85,76.90), -- RHODES
