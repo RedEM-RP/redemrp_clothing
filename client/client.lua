9e21aa8f3ef0513a380268e1c7beb84e2629b282
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-01-04 18:13:27 +0100

    However, it is needed (;

--- client/client.lua
+++ client/client.lua
@@ -641,6 +641,15 @@ function ladowanie(hash)
     --print(hash)
 end
 
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+    local str = CreateVarString(10, "LITERAL_STRING", str, Citizen.ResultAsLong())
+    SetTextScale(w, h)
+    SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+    SetTextCentre(centre)
+    if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+	Citizen.InvokeNative(0xADA9255D, 10);
+	DisplayText(str, x, y)
+end
 
 function destory()
     SetCamActive(cam, false)
