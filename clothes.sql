bc47190e172a0786c5175c307c56fc2453f8d986
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-07-02 17:55:47 +0200

    update

--- clothes.sql
+++ clothes.sql
@@ -19,6 +19,13 @@ CREATE TABLE `clothes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+CREATE TABLE `outfits` (
+  `identifier` varchar(40) NOT NULL,
+  `charid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `clothes` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
