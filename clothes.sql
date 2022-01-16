8da2fa4ad3f52678ab7fa17713c364526e429da3
meta-hub <56391287+meta-hub@users.noreply.github.com>
2022-01-16 16:32:46 +1030

    Update clothes.sql
    Modify identifier column length, allowing 'license' identifier usage

--- clothes.sql
+++ clothes.sql
@@ -13,14 +13,14 @@ SET time_zone = "+00:00";
 
 
 CREATE TABLE `clothes` (
-  `identifier` varchar(40) NOT NULL,
+  `identifier` varchar(50) NOT NULL,
   `charid` int(11) NOT NULL,
   `clothes` varchar(5000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `outfits` (
-  `identifier` varchar(40) NOT NULL,
+  `identifier` varchar(50) NOT NULL,
   `charid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `clothes` varchar(5000) NOT NULL
